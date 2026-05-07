bc2b317ec52f69a74963304e94e62bf1b48c758c
stussytt <nluzin834@gmail.com>
2026-05-07 19:59:15 +0500

    Lab3(Исправленная)

--- src/js/ui/components/initFooter.js
+++
@@ -1,8 +0,0 @@
-import footerData from "../../mockData/footerData.js";
-import { footerTemplate } from "../templates/footerTemplate.js";
-
-const initFooter = (footerNode) => {
-  footerNode.insertAdjacentHTML("beforeend", footerTemplate(footerData));
-};
-
-export default initFooter;
